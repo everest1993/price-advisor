a8e55ec254a2768fc58d02021fa6392d5110c080
everest1993 <luigi.vannozzi@protonmail.com>
2025-12-01 18:18:35 +0100

    linear regression, pulizia dati e updates scraper

--- price-advisor/src/data_processing/data_processer.py
+++ price-advisor/src/data_processing/data_processer.py
@@ -1,3 +1,9 @@
+"""
+Logica per eseguire conversioni e pulire i dati
+"""
+
+import numpy as np
+
 class DataProcesser():
     def to_float(self, text_price: str) -> float:
         """
@@ -9,3 +15,22 @@ class DataProcesser():
         price = float(price.replace(".", "").replace(",", "."))
 
         return price
+    
+
+    def clean_data_from_outliers(self, x: np.ndarray, q_low: float = 25.0, 
+                                 q_hi: float = 75.0, k: float = 1.5):
+        """
+        Rimuove gli outliers dal dataset
+        """
+        q1 = np.percentile(x, q_low)
+        q3 = np.percentile(x, q_hi)
+        iqr = q3 - q1
+
+        lower_bound = q1 - k * iqr
+        upper_bound = q3 + k * iqr
+
+        mask = (x >= lower_bound) & (x <= upper_bound)
+
+        x_cleaned = x[mask]
+
+        return x_cleaned
--- price-advisor/src/main.py
+++ price-advisor/src/main.py
@@ -1,11 +1,30 @@
 from scraping.scraper import webScraper
+from data_processing.data_processer import DataProcesser
 from config import URL
 
+import numpy as np
+
 scraper = webScraper(10)
+processer = DataProcesser()
 
 # URL, nome prodotto, condizione (opzionale: nuovo, usato)
-items = scraper.navigate_and_scrape(URL, "iphone 15", "usato")
+items = scraper.navigate_and_scrape(URL, "charizard psa 10", "usato")
+
+prices = None 
+
 if items:
     items.sort(key=lambda item: item["price"])
+    
+    # conversione in np.ndarray
+    prices = np.array([item["price"] for item in items])
 
-print(items)
+# chatgpt deve decidere gli elementi della lista che sono coerenti con la ricerca: no cover, accessori ecc
+# chatgpt deve dare un voto da 1 a 10 alla qualità dell'elemento basandosi sul titolo (o accesso descrizione)
+
+# cleaning dei dati dagli outliers (quantili di default 25, 75)
+prices_cleaned = processer.clean_data_from_outliers(prices)
+
+# regressione lineare per restituire il voto consigliato
+
+print(prices)
+print(prices_cleaned)
